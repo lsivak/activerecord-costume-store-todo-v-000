c4de77659f7b6013422f523565eb28d267231510
Lara Sanders <larasanders@gmail.com>
2017-12-05 20:27:27 +0000

    automatically backed up by learn

---
+++ db/migrate/002_create_costume_store.rb
@@ -0,0 +1,14 @@
+class CreateCostumeStores < ActiveRecord::Migration[4.2]
+
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :num_of_empolyees
+      t.string :still_in_business
+      t.integer :opening_time
+      t.integer :closing_time
+    end
+  end
+end
