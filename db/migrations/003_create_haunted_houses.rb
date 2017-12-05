41fce618dba16c5dba6ee1bcfdb668cf305d3eb8
Lara Sanders <larasanders@gmail.com>
2017-12-05 20:43:07 +0000

    automatically backed up by learn

--- db/migrations/003_create_haunted_houses.rb
+++ db/migrations/003_create_haunted_houses.rb
@@ -6,10 +6,10 @@ class CreateHauntedHouses < ActiveRecord::Migration
       t.string :location
       t.string :theme
       t.float :price
-      t.string :family_friendly
+      t.boolean :family_friendly
       t.datetime :opening_date
       t.datetime :closing_date
-      t.string :long_description
+      t.text :long_description
     end
   end
 end
