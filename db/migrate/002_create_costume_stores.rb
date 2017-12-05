15b23f2554e37075efdd301a04775b9ba9a6b60c
Lara Sanders <larasanders@gmail.com>
2017-12-05 19:24:30 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -6,7 +6,7 @@ class CreateCostumeStore < ActiveRecord::Migration
       t.string :location
       t.integer :costume_inventory
       t.integer :number_of_empolyees
-      t.string :whether_or_not_its_still_in_business
+      t.string :still_in_business
       t.integer :opening_time
       t.integer :closing_time
     end
